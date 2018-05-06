7135d3d4abbbde702c9602139d5547aa1886e6eb
tesujiro <tesujiro@gmail.com>
2018-05-06 20:06:26 +0900

    add: save latest tweet id to file

--- main.go
+++ main.go
@@ -23,29 +23,18 @@ mainloop:
 		case <-tick:
 			fmt.Printf("twitter.search %s\n", time.Now())
 			for i, tweet := range t.search() {
-				createdAt, err := tweet.CreatedAtTime()
-				if err != nil {
-					panic(err)
-				}
-				if !createdAt.After(t.startedAt) {
-					continue
-				}
 				fmt.Printf("key:%d\tid:%d\tCreatedAt:%s\tUser.ScreenName:%s\n", i, tweet.Id, tweet.CreatedAt, tweet.User.ScreenName)
 				fmt.Println("=============================================")
 				fmt.Println(tweet.Text)
 				fmt.Println("=============================================")
-				fmt.Printf("%s\n", tweet)
-				fmt.Println("=============================================")
+				//fmt.Printf("%s\n", tweet)
+				//fmt.Println("=============================================")
 				if tweet.RetweetCount == 0 {
 					fmt.Printf("==>exec\n")
-					//t.retweet(tweet.Id, true)
 					cmd := strings.Replace(tweet.Text, t.hashtag, "", -1)
-					//cmd = strings.TrimRight(cmd, "\n")
 					result := execOnContainer(ctx, cmd)
 					t.quotedTweet(result, &tweet)
 				}
-				//fmt.Println(tweet.Text)
-				//fmt.Println(tweet)
 			}
 		}
 	}
--- twitter.go
+++ twitter.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os"
-	"time"
+	"path/filepath"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
 const TWEET_MAX_CHARS = 180
 
+type savedata struct {
+	LatestId int64 `json:"latest_id"`
+}
+
 type Twitter struct {
-	api        *anaconda.TwitterApi
-	startedAt  time.Time
-	hashtag    string
-	searchedId int64
+	api *anaconda.TwitterApi
+	//startedAt time.Time
+	hashtag  string
+	savefile string // json tmp file path
+	savedata *savedata
 }
 
 func GetTwitterApi() *anaconda.TwitterApi {
@@ -26,28 +33,68 @@ func GetTwitterApi() *anaconda.TwitterApi {
 }
 
 func newTwitter() *Twitter {
-	if os.Getenv("HASHTAG") == "" {
+	hash := os.Getenv("HASHTAG")
+	if hash == "" {
 		fmt.Println("No Environment Variable (HASHTAG,TWITTER_CONSUMER_KEY,TWITTER_CONSUMER_SECRET,TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET)")
 		os.Exit(1)
 	}
-	return &Twitter{
-		api:       GetTwitterApi(),
-		hashtag:   os.Getenv("HASHTAG"),
-		startedAt: time.Now(),
+
+	savefile := filepath.Join("/tmp", hash+".json") //TODO:
+	t := Twitter{
+		api:      GetTwitterApi(),
+		hashtag:  hash,
+		savefile: savefile,
+		//startedAt: time.Now(),
 	}
+	if err := t.readSavedata(); err != nil {
+		panic(err)
+	}
+	return &t
+}
+
+func (t *Twitter) readSavedata() error {
+	raw, _ := ioutil.ReadFile(t.savefile)
+
+	var sd savedata
+	if err := json.Unmarshal(raw, &sd); err != nil {
+		t.savedata = &sd
+		//return err
+	}
+	t.savedata = &sd
+
+	return nil
+}
+
+func (t *Twitter) writeSavedata() error {
+	var js []byte
+	js, err := json.Marshal(t.savedata)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(t.savefile, js, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (t *Twitter) search() []anaconda.Tweet {
 	v := url.Values{}
-	if t.searchedId != 0 {
-		v.Add("since_id", fmt.Sprint(t.searchedId))
+	if t.savedata.LatestId != 0 {
+		fmt.Printf("since_id=%d\n", t.savedata.LatestId)
+		v.Add("since_id", fmt.Sprintf("%d", t.savedata.LatestId+1))
 	}
 	searchResult, err := t.api.GetSearch(t.hashtag, v)
 	if err != nil {
 		panic(err)
 	}
 	if len(searchResult.Statuses) > 0 {
-		t.searchedId = searchResult.Metadata.MaxId
+		t.savedata.LatestId = searchResult.Metadata.MaxId
+		fmt.Printf("t.savedata.LatestId =%d\n", t.savedata.LatestId)
+		if err := t.writeSavedata(); err != nil {
+			panic(err)
+		}
 	}
 	return searchResult.Statuses
 }
