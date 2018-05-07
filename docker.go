1af67ba9363de56a07333b432b436e3fbd9832cf
tesujiro <tesujiro@gmail.com>
2018-05-07 23:21:31 +0900

    refact docker goroutinized

--- docker.go
+++ docker.go
@@ -10,117 +10,154 @@ import (
 	"github.com/docker/docker/client"
 )
 
-//func main() {
-func execOnContainer(ctx context.Context, cmd string) string {
+type instance struct {
+	cmdCh    chan string
+	resultCh chan string
+	errCh    chan error
+}
 
-	cli, err := client.NewClientWithOpts()
-	if err != nil {
-		//panic(err)
-		fmt.Printf("Container New Client ERROR: %v\n", err)
-		return fmt.Sprintf("%v", err)
+func newDockerContainer() *instance {
+	c := instance{
+		cmdCh:    make(chan string),
+		resultCh: make(chan string),
+		errCh:    make(chan error),
 	}
+	return &c
+}
 
-	/*
-		reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+func (c *instance) exec(cmd string) error {
+	c.cmdCh <- cmd
+	return nil
+}
+
+func (c *instance) exit() (string, error) {
+	close(c.cmdCh)
+	//close(c.resultCh)
+	//TODO:
+	return <-c.resultCh, nil
+}
+
+//func execOnContainer(ctx context.Context, cmd string) string {
+func (c *instance) run(ctx context.Context) error {
+
+	go func() {
+		cli, err := client.NewClientWithOpts()
 		if err != nil {
 			//panic(err)
-			fmt.Printf("Container Image Pull ERROR: %v\n", err)
-			return fmt.Sprintf("%v", err)
+			fmt.Printf("Container New Client ERROR: %v\n", err)
+			//return err
 		}
-		io.Copy(os.Stdout, reader)
-	*/
 
-	// Create Container
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		//Image:        "alpine",
-		//Cmd:          []string{"/bin/ash"},
-		Image:        "centos",
-		Cmd:          []string{"/bin/bash"},
-		OpenStdin:    true,
-		StdinOnce:    true,
-		AttachStdin:  true,
-		AttachStdout: true,
-		AttachStderr: true,
-		Tty:          false,
-	}, nil, nil, "")
-	if err != nil {
-		//panic(err)
-		fmt.Printf("Container Create ERROR: %v\n", err)
-		return fmt.Sprintf("%v", err)
-	}
+		/*
+			reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+			if err != nil {
+				//panic(err)
+				fmt.Printf("Container Image Pull ERROR: %v\n", err)
+				return fmt.Sprintf("%v", err)
+			}
+			io.Copy(os.Stdout, reader)
+		*/
 
-	defer func() {
-		if err := cli.ContainerRemove(context.Background(), resp.ID, types.ContainerRemoveOptions{}); err != nil {
-			fmt.Printf("ContainerRemove ERROR: %v\n", err)
-		}
-	}()
-
-	// Start Container
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		fmt.Printf("Container Start ERROR: %v\n", err)
-		return fmt.Sprintf("%v", err)
-	}
-	defer func() {
-		if err := cli.ContainerStop(context.Background(), resp.ID, nil); err != nil {
-			fmt.Printf("ContainerStop ERROR: %v\n", err)
-		}
-	}()
-
-	fmt.Printf("Container Started\n")
-
-	// Attach Container
-	hjConn, err := cli.ContainerAttach(ctx, resp.ID, types.ContainerAttachOptions{
-		Stream: true,
-		Stdin:  true,
-		Stdout: true,
-		Stderr: true,
-		Logs:   false,
-	})
-	if err != nil {
-		fmt.Printf("Container Attach ERROR: %v\n", err)
-		return fmt.Sprintf("%v", err)
-	}
-	defer hjConn.Close()
-	fmt.Printf("Container Attached\n")
-
-	// Exec Commands
-	//io.WriteString(os.Stdin, cmd)
-	//os.Stdin.Closefalse()
-	cmd = fmt.Sprintf("%s\nexit\n", cmd)
-	hjConn.Conn.Write([]byte(cmd))
-
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
+		// Create Container
+		resp, err := cli.ContainerCreate(ctx, &container.Config{
+			//Image:        "alpine",
+			//Cmd:          []string{"/bin/ash"},
+			Image:        "centos",
+			Cmd:          []string{"/bin/bash"},
+			OpenStdin:    true,
+			StdinOnce:    true,
+			AttachStdin:  true,
+			AttachStdout: true,
+			AttachStderr: true,
+			Tty:          false,
+		}, nil, nil, "")
 		if err != nil {
 			//panic(err)
-			fmt.Printf("Container errCh ERROR: %v\n", err)
-			return fmt.Sprintf("%v", err)
+			fmt.Printf("Container Create ERROR: %v\n", err)
+			//return err
 		}
-	case <-statusCh:
-	}
-	fmt.Printf("Container Wait Finished\n")
 
-	b, err := ioutil.ReadAll(hjConn.Conn)
-	if err != nil {
-		fmt.Printf("Container Read ERROR: %v\n", err)
-		return fmt.Sprintf("%v", err)
-	}
-	result := string(b)[8:]
-	fmt.Println(result)
-	return result
+		defer func() {
+			if err := cli.ContainerRemove(context.Background(), resp.ID, types.ContainerRemoveOptions{}); err != nil {
+				fmt.Printf("ContainerRemove ERROR: %v\n", err)
+			}
+		}()
 
-	// get log
-	/*
-		out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+		// Start Container
+		if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+			fmt.Printf("Container Start ERROR: %v\n", err)
+			//return err
+		}
+		defer func() {
+			if err := cli.ContainerStop(context.Background(), resp.ID, nil); err != nil {
+				fmt.Printf("ContainerStop ERROR: %v\n", err)
+			}
+		}()
+
+		fmt.Printf("Container Started\n")
+
+		// Attach Container
+		hjConn, err := cli.ContainerAttach(ctx, resp.ID, types.ContainerAttachOptions{
+			Stream: true,
+			Stdin:  true,
+			Stdout: true,
+			Stderr: true,
+			Logs:   false,
+		})
 		if err != nil {
-			panic(err)
+			fmt.Printf("Container Attach ERROR: %v\n", err)
+			//return err
 		}
-		//io.Copy(os.Stdout, out)
+		defer hjConn.Close()
+		fmt.Printf("Container Attached\n")
 
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(out)
-		result := buf.String()
+		// Exec Commands
+		//io.WriteString(os.Stdin, cmd)
+		//os.Stdin.Closefalse()
+		/*
+			cmd = fmt.Sprintf("%s\nexit\n", cmd)
+			hjConn.Conn.Write([]byte(cmd))
+		*/
+		//var wg sync.WaitGroup
+		//wg.Add(1)
+		for cmd := range c.cmdCh {
+			fmt.Printf("cmd received:%v\n", cmd)
+			cmd = fmt.Sprintf("%s\nexit\n", cmd)
+			hjConn.Conn.Write([]byte(cmd))
+		}
+
+		statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+		select {
+		case err := <-errCh:
+			if err != nil {
+				//panic(err)
+				fmt.Printf("Container errCh ERROR: %v\n", err)
+				//return err
+			}
+		case <-statusCh:
+		}
+		fmt.Printf("Container Wait Finished\n")
+		b, err := ioutil.ReadAll(hjConn.Conn)
+		if err != nil {
+			fmt.Printf("Container Read ERROR: %v\n", err)
+		}
+		//result := string(b)[8:]
+		result := string(b)
+		fmt.Println("result:" + result)
+		c.resultCh <- result
+	}()
+
+	/*
+		b, err := ioutil.ReadAll(hjConn.Conn)
+		if err != nil {
+			fmt.Printf("Container Read ERROR: %v\n", err)
+			return err
+		}
+		result := string(b)[8:]
+		fmt.Println(result)
+		return result
 	*/
+	//wg.Wait()
+	return nil
 
 }
--- main.go
+++ main.go
@@ -29,10 +29,28 @@ mainloop:
 				fmt.Println("=============================================")
 				//fmt.Printf("%s\n", tweet)
 				//fmt.Println("=============================================")
-				if tweet.RetweetCount == 0 {
+				if tweet.RetweetCount == 0 { //TODO: remove
 					fmt.Printf("==>exec\n")
 					cmd := strings.Replace(tweet.Text, t.hashtag, "", -1)
-					result := execOnContainer(ctx, cmd)
+					//result := execOnContainer(ctx, cmd)
+					d := newDockerContainer()
+					if err := d.run(ctx); err != nil {
+						result := fmt.Sprintf("%v", err)
+						t.quotedTweet(result, &tweet)
+						panic(err)
+					}
+					if err := d.exec(cmd); err != nil {
+						result, _ := d.exit()
+						result += fmt.Sprintf("%v", err)
+						t.quotedTweet(result, &tweet)
+						panic(err)
+					}
+					result, err := d.exit()
+					if err != nil {
+						result += fmt.Sprintf("%v", err)
+						t.quotedTweet(result, &tweet)
+						panic(err)
+					}
 					t.quotedTweet(result, &tweet)
 				}
 			}
